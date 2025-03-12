f17d551851bd36fef375dc213d52434804f68dd3
devjiwonchoi <devjiwonchoi@gmail.com>
2025-03-12 19:11:26 +0100

    wrap with main

--- scripts/commit-if-has-change.mjs
+++ scripts/commit-if-has-change.mjs
@@ -1,34 +1,39 @@
 import { execSync } from 'child_process'
 
-const version = require('../packages/main/package.json').version
-const githubToken = process.env.RELEASE_BOT_GITHUB_TOKEN
 
-if (!githubToken) {
-  console.log(`Missing RELEASE_BOT_GITHUB_TOKEN`)
-  return
-}
+function main() {
+  const version = require('../packages/main/package.json').version
+  const githubToken = process.env.RELEASE_BOT_GITHUB_TOKEN
 
-const args = process.argv
-const message = args[args.indexOf('--message') + 1]
-
-try {
-  const status = execSync('git status --porcelain').toString()
-  if (!status) {
-    console.log('No changes to commit.')
+  if (!githubToken) {
+    console.log(`Missing RELEASE_BOT_GITHUB_TOKEN`)
     return
   }
 
-  // Update for lockfile diff.
-  execSync('pnpm install')
+  const args = process.argv
+  const message = args[args.indexOf('--message') + 1]
 
-  execSync(
-    `git remote set-url origin https://devjiwonchoi:${githubToken}@github.com/devjiwonchoi/lab-releases.git`
-  )
-  execSync('git config user.name "devjiwonchoi"')
-  execSync('git config user.email "devjiwonchoi@gmail.com"')
-  execSync('git add .')
-  execSync(`git commit -m "${message || `v${version}`}"`)
-  execSync('git push')
-} catch (error) {
-  console.error('Error during git operations:', error)
+  try {
+    const status = execSync('git status --porcelain').toString()
+    if (!status) {
+      console.log('No changes to commit.')
+      return
+    }
+
+    // Update for lockfile diff.
+    execSync('pnpm install')
+
+    execSync(
+      `git remote set-url origin https://devjiwonchoi:${githubToken}@github.com/devjiwonchoi/lab-releases.git`
+    )
+    execSync('git config user.name "devjiwonchoi"')
+    execSync('git config user.email "devjiwonchoi@gmail.com"')
+    execSync('git add .')
+    execSync(`git commit -m "${message || `v${version}`}"`)
+    execSync('git push')
+  } catch (error) {
+    console.error('Error during git operations:', error)
+  }
 }
+
+main()
