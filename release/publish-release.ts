e453610558ed9ada00d6717fec8ce3b0904a1337
devjiwonchoi <devjiwonchoi@gmail.com>
2025-05-10 22:57:19 +0200

    yea

--- release/publish-release.ts
+++
@@ -1,8 +0,0 @@
-import { exec } from './utils'
-
-export default async function publishRelease() {
-  await exec('pnpm tsx ./publish-npm.ts')
-  await exec('pnpm tsx ./publish-release-note.ts')
-}
-
-publishRelease()
