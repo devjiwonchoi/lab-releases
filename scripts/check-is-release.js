ee3ff723ee685261224b30faf09bc56e8b1b4913
devjiwonchoi <devjiwonchoi@gmail.com>
2025-05-13 19:35:33 +0200

    Version Packages (canary)

---
+++ scripts/check-is-release.js
@@ -0,0 +1,31 @@
+const { execSync } = require('child_process')
+
+const checkIsRelease = async () => {
+  let commitId = process.argv[2] || ''
+
+  // parse only the last string which should be version if
+  // it's a publish commit
+  const commitMsg = execSync(
+    `git log -n 1 --pretty='format:%B'${commitId ? ` ${commitId}` : ''}`
+  )
+    .toString()
+    .trim()
+
+  const versionString = commitMsg.split(' ').pop().trim()
+  const publishMsgRegex = /^v\d{1,}\.\d{1,}\.\d{1,}(-\w{1,}\.\d{1,})?$/
+  const newPublishMsgRegex =
+    /^Version Packages (canary|release-candidate|stable)$/
+
+  if (
+    publishMsgRegex.test(versionString) ||
+    newPublishMsgRegex.test(versionString)
+  ) {
+    console.log(versionString)
+    process.exit(0)
+  } else {
+    console.log('not publish commit', { commitId, commitMsg, versionString })
+    process.exit(1)
+  }
+}
+
+checkIsRelease()
